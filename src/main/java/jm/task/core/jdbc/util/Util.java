862b516a33fef1130c63cb15835a8dd06e3ee2dc
ASMAXI <108028037+ASMAXI@users.noreply.github.com>
2024-07-31 20:02:48 +0300

    commit

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,36 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Logger;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static final Logger LOGGER = Logger.getLogger(Util.class.getName());
+
+    private static final String DB_HOST = "localhost";
+    private static final String DB_NAME = "my_connection";
+    private static final String DB_PORT = "3306";
+    private static final String DB_USER = "bestuser";
+    private static final String DB_PASS = "bestuser";
+
+    private static Util instance;
+    private Connection dbConnection;
+
+    private Util() {}
+
+    public static synchronized Util getInstance() {
+        if (instance == null) {
+            instance = new Util();
+        }
+        return instance;
+    }
+
+    public Connection getDbConnection() throws SQLException {
+        if (dbConnection == null || dbConnection.isClosed()) {
+            String connectionString = "jdbc:mysql://" + DB_HOST + ":" + DB_PORT + "/" + DB_NAME;
+            dbConnection = DriverManager.getConnection(connectionString, DB_USER, DB_PASS);
+        }
+        return dbConnection;
+    }
 }
